86e3a2d701c429c5455606dce5cbb6f7fcd02455
unknown <ciglesiasvera@gmail.com>
2024-10-01 14:26:29 -0300

    commit ej de comprobacion modulo 6 sesion 4

---
+++ site_django/book/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse  
+
+def index(request):
+    return HttpResponse("Bienvenidos a mi sitio de libros")  
+
+def home(request):
+    return redirect('/admin/')
